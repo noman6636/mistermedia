286875f6e167e3ab75b0beb6fdb0701a6e835787
Noman Mustafa <nomandeveloper6636@gmail.com>
2026-07-17 08:32:25 -0400

    remove dtask

--- dtask/application/helpers/about.php
+++
@@ -1,4 +0,0 @@
-<?php
-// This file was neutralized because an obfuscated payload was detected.
-// Keep it inert unless a trusted, reviewed helper implementation is required.
-return;
--- dtask/cgi-bin/form.php
+++
@@ -1,5 +0,0 @@
-яШяв<?php 
-/**
-* Note: This file may contain artifacts of previous malicious infection.
-* However, the dangerous code has been removed, and the file is now safe to use.
-*/
